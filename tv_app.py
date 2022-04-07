4303307e5072c38621812c84869e12123abb7348
pointerboy <lukatrbovic@outlook.com>
2022-04-07 14:36:39 +0200

    Breaking changes to TV App!
    - The TV app now starsts the web server.
    - Added a new address for the website
    - Increased refresh time

--- tv_app.py
+++ tv_app.py
@@ -1,20 +1,22 @@
 import webview
+import os 
 import time
 
-web_address = 'http://127.0.01:5000'
+web_address = 'http://192.168.0.13:5000'
 
-def refresh_webview():
+
+def refresh_webview(window):
+    window.toggle_fullscreen()
     while True:
-        time.sleep(180)
+        time.sleep(120)
         window.load_url(web_address)        
 
-
-def toggle_fullscreen(window):
-    time.sleep(1.0)
-
-    window.toggle_fullscreen()
-
-
 if __name__ == '__main__':
+    time.sleep(5)
+
+    os.system("python3 ~/production/tradium_tv/webapp/app.py &")
+    
+    time.sleep(10) # wait for server to fully start
+    
     window = webview.create_window('Tradium TV', web_address)
-    webview.start(toggle_fullscreen, window)
+    webview.start(refresh_webview, window)
